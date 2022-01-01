6217b0c5f83982bbee37a5116912cd8f588c48cd
Zach Taggart <ztaggart020701@gmail.com>
2022-01-01 15:09:10 -0500

    added node cors

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const http = require('http')
-
+const cors = require('cors')
 const express = require('express')
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 let recipes = [
     {
